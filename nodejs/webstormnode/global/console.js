ecbacb316b27848c276b8167704a6f88dffc69c2
qiansimin88 <348867341@qq.com>
2015-10-16 17:55:50 +0800

    那么

---
+++ nodejs/webstormnode/global/console.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Administrator on 2015/10/16.
+ */
+/*
+*
+* 控制台是一个界面
+* */
+
+
+console.log('this is a log');
---
+++ nodejs/webstormnode/global/global.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Administrator on 2015/10/16.
+ */
+//console.log(global);
+
+console.log(__filename);  //当前脚本的绝对路径 e:\wodezhandian\nodejs\webstormnode\global\global.js
+
+console.log(__dirname);   //当前脚本的目录  e:\wodezhandian\nodejs\webstormnode\global
