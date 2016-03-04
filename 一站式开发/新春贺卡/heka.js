35470410c9428a740aea61188582b47b23993434
qiansimin88 <348867341@qq.com>
2016-03-04 18:34:23 +0800

    add some

--- 一站式开发/新春贺卡/heka.js
+++ 一站式开发/新春贺卡/heka.js
@@ -4,7 +4,13 @@
 
 		var music = document.getElementById('music'),
 
-		audio = document.getElementsByTagName('audio')[0];
+		audio = document.getElementsByTagName('audio')[0],
+
+		page1 = document.getElementById('page1'),
+
+		page2 = document.getElementById('page2'),
+
+		page3 = document.getElementById('page3');
 
 		//音乐暂停和播放
 		music.addEventListener('touchstart',function(){
@@ -34,8 +40,28 @@
 
 			music.setAttribute('class','');   //放完就清除play标签
 
-		},false)	
+		},false);
 
-	}
+		page1.addEventListener('touchstart',function(){
+
+			this.style.display = 'none';
+
+			page2.style.display = 'block';
+
+			page3.style.display = 'block';
+
+			page3.style.top = '100%';
+
+			setTimeout(function(){
+
+				page2.className = 'page fadeOut';
+
+				page3.className = 'page fadeIn';
+
+			},5500)
+
+		},false);
+
+	};
 
 })();
